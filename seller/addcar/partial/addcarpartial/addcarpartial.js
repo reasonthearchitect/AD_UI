94c74b0a822003b9a7e49ded640b7ffe23abf45f
reasonthearchitect <rhys.personal@gmail.com>
2016-07-30 19:55:21 -0400

    Added the http post for adding a car.

--- seller/addcar/partial/addcarpartial/addcarpartial.js
+++ seller/addcar/partial/addcarpartial/addcarpartial.js
@@ -1,6 +1,26 @@
-angular.module('addcar').controller('AddcarpartialCtrl',function($scope, $stateParams){
+angular.module('addcar').controller('AddcarpartialCtrl',function($scope, $stateParams, $http){
 
 	$scope.newcar = {"seller": $stateParams.id};
-	
 
+	$scope.clear = function() {
+		$scope.newcar = {"seller": $stateParams.id};
+	};
+
+	$scope.addcar = function() {
+
+		$http({
+  			method: 'POST',
+  			url: '/addcar/',
+  			data: $scope.newcar
+		}).then(function successCallback(response) {
+    		console.log("Success");
+    		console.log(JSON.stringify(response));
+  			
+  			$scope.clear();
+  		}, function errorCallback(response) {
+    		console.log("ERROR????????????");
+    		console.log(JSON.stringify(response));
+    	});
+		
+	}
 });
