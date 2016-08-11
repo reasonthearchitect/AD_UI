26f3ffe95148daddc990a292fd1bb00df6e38236
reasonthearchitect <rhys.personal@gmail.com>
2016-08-11 18:17:54 -0400

    Adding form vaidation to the car seller add car screen

--- seller/addcar/partial/addcarpartial/addcarpartial.js
+++ seller/addcar/partial/addcarpartial/addcarpartial.js
@@ -2,20 +2,19 @@ angular.module('addcar').controller('AddcarpartialCtrl',function($scope, $stateP
 
 	$scope.newcar = {"seller": $stateParams.id};
 
+	$scope.required = true;
+
 	$scope.clear = function() {
 		$location.path('/sellerslist/' + $stateParams.id);
-		//$scope.newcar = {"seller": $stateParams.id};
 	};
 
 	$scope.addcar = function() {
-
 		$http({
   			method: 'POST',
   			url: '/addcar/',
   			data: $scope.newcar
 		}).then(function successCallback(response) {
 			$location.path('/sellerslist/' + $stateParams.id)
-    		//$scope.clear();
   		}, function errorCallback(response) {
     		console.log(JSON.stringify(response));
     	});
